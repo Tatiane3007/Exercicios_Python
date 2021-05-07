ff8b7401f598e2b84d46e260915e015b5fe99dc9
Tatiane Silva <50220046+Tatiane3007@users.noreply.github.com>
2021-05-07 16:37:47 -0300

    Create 2_dir_e_help.py

---
+++ 2_dir_e_help.py
@@ -0,0 +1,14 @@
+"""
+Utilitários Python para auxiliar na programação.
+
+dir -> Apresenta todos os atributos/propriedades e funções/métodos disponíveis para determinado tipo de dados/variável
+
+dir(tipo de dado/variável)
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+help -> Apresenta a documentação/como utilizar os atributos/propriedades e funções/métodos disponíveis para determinado
+        tipo de dado ou variável
+
+help(tipo de dado/variavel.propriedade)
+"""
