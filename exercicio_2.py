444690264e712220a592cbaf2ea86b62f20e9b70
Tatiane Silva <50220046+Tatiane3007@users.noreply.github.com>
2021-05-08 15:59:08 -0300

    Create exercicio_2.py

---
+++ exercicio_2.py
@@ -0,0 +1,11 @@
+# 2 - Faça um programa que leia um número real e o imprima.
+
+def real():
+    num = float(input("Digite um numero real: "))
+    print(f"O número digitado foi {num}!")
+
+def main():
+    real()
+
+if __name__ == "__main__":
+    main()
