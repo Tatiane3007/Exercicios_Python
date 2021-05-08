524605056b173ca179896b1f4686f328f8aa9b73
Tatiane Silva <50220046+Tatiane3007@users.noreply.github.com>
2021-05-08 15:51:31 -0300

    Create exercicios_1.py

---
+++ exercicios_1.py
@@ -0,0 +1,11 @@
+# 1 - Faça um programa que leia um número inteiro e o imprima.
+
+def inteiro():
+    num = input("Digite um numero: ")
+    print(f"O número digitado foi {num}!")
+
+def main():
+    inteiro()
+
+if __name__ == "__main__":
+    main()
