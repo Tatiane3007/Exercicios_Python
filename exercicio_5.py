5e40021ab4f60fa4908ca2d0348669e065812d86
Tatiane Silva <50220046+Tatiane3007@users.noreply.github.com>
2021-05-08 16:12:07 -0300

    Create exercicio_5.py

---
+++ exercicio_5.py
@@ -0,0 +1,11 @@
+# 5 - Leia um número real e imprima a quinta parte desse número.
+
+def quinta_parte():
+    num = float(input("Digite um número real: "))
+    print(f"A quinta parte desse número é {num / 5}!")
+
+def main():
+    quinta_parte()
+
+if __name__ == "__main__":
+    main()
